a4b4e36319fbb1105b4a703779b48904c639dc9c
pecopeco <yangpeco@qq.com>
2019-12-17 20:32:59 +0800

    update

--- src/content/index.js
+++ src/content/index.js
@@ -1,5 +1,3 @@
-console.log('This is content!')
-
 document.ondragend = function (e) {
   if (e.target.tagName === 'A' && e.target.getAttribute("href")) {
     window.open(e.target.getAttribute("href"), "_blank")
