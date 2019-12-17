1929082f9dc2e1a307bdfbf08a019718e7fba140
pecopeco <yangpeco@qq.com>
2019-12-17 14:23:15 +0800

    update

--- src/content/index.js
+++ src/content/index.js
@@ -1,9 +1,6 @@
 console.log('This is content!')
 
-let target, x, y
-
 document.ondragend = function (e) {
-  console.log('拖动结束', e.target.tagName === 'A')
   if (e.target.tagName === 'A' && e.target.getAttribute("href")) {
     window.open(e.target.getAttribute("href"), "_blank")
   }
